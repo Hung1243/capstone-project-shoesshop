f465eadd57d524296e265723fd23aff3335c207c
An Tran <tranvinhthuyan9797@gmail.com>
2024-01-25 22:01:22 +0700

    update search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useFormik } from "formik";
+import { orderBy } from "lodash";
 import React, { useEffect, useState } from "react";
 import { NavLink, useSearchParams } from "react-router-dom";
+
 //Gõ text => url (setSearchParams)
 //Get từ url => về component gọi api (searchParams.get('key'))
 const Search = () => {
@@ -21,12 +23,15 @@ const Search = () => {
       });
     },
   });
+  const [sortOrder, setSortOrder] = useState("asc");
+
   const getProductByKeyword = async () => {
     //Gọi api
     const res = await axios({
       url: `https://shop.cyberlearn.vn/api/Product?keyword=${tuKhoa}`,
       method: "GET",
     });
+
     setArrProduct(res.data.content);
   };
   useEffect(() => {
@@ -37,11 +42,20 @@ const Search = () => {
     getProductByKeyword();
   }, [tuKhoa]);
 
+  const handleSort = (order) => {
+    setSortOrder(order);
+
+    // Sort the array of products based on the selected order
+    const sortedProducts = orderBy(arrProduct, ["price"], [order]);
+    setArrProduct(sortedProducts);
+  };
+
   return (
     <div className="container">
       <form className="frm-search mt-2" onSubmit={formSearch.handleSubmit}>
         <div className="input-group mb-3">
           <button className="input-group-button btn btn-success">Search</button>
+
           <div className="form-floating">
             <input
               type="text"
@@ -57,6 +71,22 @@ const Search = () => {
         </div>
       </form>
       <h3 className="my-2">Search result</h3>
+      <div className="sorting-bar">
+        <label className=" me-2" htmlFor="sortOrder">
+          Sort by:{" "}
+        </label>
+        <select
+          id="sortOrder"
+          value={sortOrder}
+          onChange={(e) => handleSort(e.target.value)}
+          className=""
+          style={{ fontSize: "14px", cursor: "pointer" }}
+        >
+          <option value="asc">Price, Low to High</option>
+          <option value="desc">Price, High to Low</option>
+        </select>
+      </div>
+
       <div className="row">
         {arrProduct.map((prod) => {
           return (
